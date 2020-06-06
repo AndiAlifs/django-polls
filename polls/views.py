655e7600bf92e44ec0ebb0185bd0220bcd79a226
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-06-06 16:21:52 +0800

    update polls

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render,get_object_or_404
 from django.http import Http404
+
 from .models import Question
 
 def index(request):
